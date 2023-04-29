7a7628e3cbf77ed681ad5b30834ac10b3c35a000
kairavsheth <85606266+kairavsheth@users.noreply.github.com>
2023-04-30 01:28:17 +0530

    Code commit

--- youtube_search/settings.py
+++ youtube_search/settings.py
@@ -9,23 +9,27 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(DEBUG=(bool, False))
+env_file = os.path.join(BASE_DIR, ".env")
+env.read_env(env_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@=ldwr3h94kc958ivt=pef_gf2#w6)^1xmo#!xrpn&-3*5o5qt'
+SECRET_KEY = env('SECRET_KEY', default='django-insecure-@=ldwr3h94kc958ivt=pef_gf2#w6)^1xmo#!xrpn&-3*5o5qt')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'youtube',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +81,7 @@ WSGI_APPLICATION = 'youtube_search.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db()
 }
 
 
@@ -122,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+YT_API_KEY = "AIzaSyD_Yc5ZZaevusP-4bjMRTGf-acY_KGYlNc"
--- youtube_search/urls.py
+++ youtube_search/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import youtube.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/list/', youtube.views.List.as_view()),
+    path('api/fetch/', youtube.views.Fetch.as_view()),
 ]
